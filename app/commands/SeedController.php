fc8b940c48bc3f02589545677012f4c61d21c79f
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-02-21 08:39:34 -0800

    up

--- app/commands/SeedController.php
+++ app/commands/SeedController.php
@@ -45,7 +45,7 @@ class SeedController extends Controller
     
     public function actionThemes()
     {
-        $themes = require __DIR__ . '/themes.php';
+        $data = include Yii::getAlias('@commands').'/data/themes.php';
         $this->actionTruncate(['themes']);
         $this->startProgress(0, count($themes), 'Seeding Themes: ');
         foreach ($themes as $i => $theme) {
--- app/config/console.php
+++ app/config/console.php
@@ -12,6 +12,7 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
         '@tests' => '@app/tests',
+        '@commands' => '@app/commands',
     ],
     'components' => [
         'access' => ['class' => 'app\components\AccessComponent'],
