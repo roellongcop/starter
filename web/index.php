16889d7d6cea49ae19c80ee37c455d4d357fd8cf
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-04-16 21:50:50 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,12 +3,12 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-defined('APP_ENV') or define('APP_ENV', 'local');
+
+require 'functions.php';
 
 require __DIR__ . '/../app/vendor/autoload.php';
 require __DIR__ . '/../app/vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../app/config/web.php';
-require 'functions.php';
 
 (new yii\web\Application($config))->run();
