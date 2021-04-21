da6888751968aa54a2ebb794208933bcc40fd2dc
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-04-21 16:50:39 +0800

    up

--- app/widgets/views/search_button.php
+++ app/widgets/views/search_button.php
@@ -5,12 +5,7 @@ use yii\helpers\Html;
 
 <div class="text-center">
     <div class="btn btn-group btn-block">
-
-        <?= Anchor::widget([
-            'title' => 'Reset',
-            'link' => ['index'],
-            'options' => ['class' => 'btn btn-default'],
-        ]) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
     </div>
 </div>
