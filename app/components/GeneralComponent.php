e2c71116b203075a6126a27e5a5bc3c898d886c6
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-04-23 16:53:44 +0800

    Update GeneralComponent.php

--- app/components/GeneralComponent.php
+++ app/components/GeneralComponent.php
@@ -7,18 +7,6 @@ use yii\base\Component;
 
 class GeneralComponent extends Component
 {
-    public function betweenRange($date_range, $field='created_at')
-    {
-        $hours = App::date_timezone(date("Y-m-d H:i:s"), "P");
-
-        return [
-            "between", 
-            "date(DATE_ADD({$field},INTERVAL '{$hours}' HOUR_MINUTE))", 
-            App::dateRange($date_range, 'start'), 
-            App::dateRange($date_range, 'end'), 
-        ];
-    }
-
     public function getAllTables()
     {
         $tables = App::getTableNames();
