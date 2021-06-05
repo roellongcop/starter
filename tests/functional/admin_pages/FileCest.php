bb46aabf1d1b3b0afc31ef568a389909aa411311
roellongcop <30338518+roellongcop@users.noreply.github.com>
2021-06-05 15:51:36 +0800

    up

--- tests/functional/admin_pages/FileCest.php
+++ tests/functional/admin_pages/FileCest.php
@@ -1,18 +1,10 @@
 <?php
-
-use yii\helpers\StringHelper;
-use yii\helpers\Inflector;
-
-$modelClass = StringHelper::basename($generator->modelClass);
-?>
-<?= '<?php' ?>
-
 use app\models\User;
-use <?= ltrim($generator->modelClass, '\\') ?>;
+use app\models\File;
 
 // Admin page functional test cest file. Move this file to @app\tests\functional\admin_pages
 
-class <?= $modelClass ?>Cest
+class FileCest
 {
     public $user;
     public $model;
@@ -20,7 +12,7 @@ class <?= $modelClass ?>Cest
     public function _before(FunctionalTester $I)
     {
         $this->user = User::findByUsername('developer');
-        $this->model = <?= $modelClass ?>::findOne(1);
+        $this->model = File::findOne(1);
         $I->amLoggedInAs($this->user);
     }
 
@@ -32,32 +24,32 @@ class <?= $modelClass ?>Cest
     public function indexPage(FunctionalTester $I)
     {
         $I->amOnPage($this->model->getIndexUrl(false));
-        $I->see(<?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>, 'h5');
+        $I->see('Files', 'h5');
     }
 
     public function createPage(FunctionalTester $I)
     {
         $I->amOnPage($this->model->getCreateUrl(false));
-        $I->see(<?= $generator->generateString('Create '. Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>, 'h5');
+        $I->see('Create File', 'h5');
     }
 
     public function viewPage(FunctionalTester $I)
     {
         $I->amOnPage($this->model->getViewUrl(false));
-        $I->see('<?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?>:', 'h5');
+        $I->see('File:', 'h5');
     }
 
     public function updatePage(FunctionalTester $I)
     {
         $I->amOnPage($this->model->getUpdateUrl(false));
-        $I->see('Update <?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?>:', 'h5');
+        $I->see('Update File:', 'h5');
     }
 
 
     public function bulkActionPage(FunctionalTester $I)
     {
         $I->amOnPage($this->model->getIndexUrl(false));
-        $I->submitForm('form[action="/<?= Inflector::slug($modelClass) ?>/process-checkbox"]', [
+        $I->submitForm('form[action="/file/process-checkbox"]', [
             'process-selected' => 'active', 
             'selection' => [1]
         ]);
