13d26ddebf88511cf2c7348ff2a14df81021d11a
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-07-05 14:22:12 +0800

    Update _fixture-data.php

--- gii/model/starter/default/_fixture-data.php
+++ gii/model/starter/default/_fixture-data.php
@@ -1,6 +1,7 @@
 <?= "<?php\n" ?>
-return [
-	1 => [
+
+$model = new \app\helpers\FixtureData(function($params) {
+    return [
 <?php foreach ($labels as $name => $label): ?>
 <?php if (in_array($name, ['updated_by', 'created_by'])): ?>
 		<?= "'$name' => 1,\n" ?>
@@ -11,5 +12,12 @@ return [
 <?php endif ?>
 <?php endif ?>
 <?php endforeach; ?>
-	]
-];
+    ];
+});
+
+$model->add('1');
+$model->add('inactive', [], [
+	'record_status' => <?= $className ?>::RECORD_INACTIVE
+]);
+
+return $model->getData();
