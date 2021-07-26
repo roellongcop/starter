813dc70714a7b7bf0110ac9c6f4ea604c908e725
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-07-26 13:33:00 +0800

    Update params.php

--- config/params.php
+++ config/params.php
@@ -1,12 +1,10 @@
 <?php
 return [
     'pagination' => [
-        20 => 20,
+        25 => 25,
         50 => 50,
+        75 => 75,
         100 => 100,
-        250 => 250,
-        500 => 500,
-        1000 => 1000,
     ], 
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
