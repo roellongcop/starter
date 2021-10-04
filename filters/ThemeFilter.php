aadd02a9455ca8bd44451b6da6a9a03bd4f0d164
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-10-04 15:27:26 -0700

    Update ThemeFilter.php

--- filters/ThemeFilter.php
+++ filters/ThemeFilter.php
@@ -27,7 +27,7 @@ class ThemeFilter extends ActionFilter
                 $theme = App::identity('currentTheme');
             }
         }
-        $theme = $theme ?: Theme::findOne(App::generalSetting('theme'));
+        $theme = $theme ?: Theme::findOne(App::setting('system')->theme);
         $theme = $this->theme ?: $theme;
 
         if ($theme) {
