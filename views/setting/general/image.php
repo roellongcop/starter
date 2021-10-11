9e8a92a31ca3c38b6736547de062da3a9c15bd56
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-10-11 13:39:49 +0800

    Update image.php

--- views/setting/general/image.php
+++ views/setting/general/image.php
@@ -10,79 +10,50 @@ use app\widgets\ImageGallery;
 	<p class="lead">Images</p>
 	<div class="row">
 		<div class="col-md-4">
-			<div class="mw200" id="primary_logo-container">
-                <p class="text-warning">Primary Logo</p>
-                <?= Html::image($model->primary_logo, ['w' => 200], [
-                    'class' => 'img-thumbnail',
-                    'loading' => 'lazy',
-                ]) ?>
-				<?= $form->field($model, 'primary_logo')->hiddenInput()->label(false) ?>
-            </div>
+            <p class="text-warning">Primary Logo</p>
+            <?= Html::image($model->primary_logo, ['w' => 200], [
+                'class' => 'img-thumbnail primary_logo',
+                'loading' => 'lazy',
+            ]) ?>
             <?= ImageGallery::widget([
                 'model' => $model,
-                'uploadFileName' => 'Primary Logo',
+                'attribute' => 'primary_logo',
                 'ajaxSuccess' => "
                     if(s.status == 'success') {
-                        KTApp.block('#primary_logo-container', {
-                            overlayColor: '#000000',
-                            state: 'primary',
-                            message: 'Processing...'
-                        });
-                        setTimeout(function() {KTApp.unblock('#primary_logo-container');}, 1000);
-                        $('#primary_logo-container img').attr('src', s.src + '&w=200');
-                        $('#imagesettingform-primary_logo').val(s.src);
+                        $('img.primary_logo').attr('src', s.src + '&w=200');
                     }
                 ",
             ]) ?> 
 		</div>
 		<div class="col-md-4">
-            <div class="mw200" id="secondary_logo-container">
-                <p class="text-warning">Secondary Logo</p>
-                <?= Html::image($model->secondary_logo, ['w' => 200], [
-                    'class' => 'img-thumbnail',
-                    'loading' => 'lazy',
-                ]) ?>
-				<?= $form->field($model, 'secondary_logo')->hiddenInput()->label(false) ?>
-            </div>
+            <p class="text-warning">Secondary Logo</p>
+            <?= Html::image($model->secondary_logo, ['w' => 200], [
+                'class' => 'img-thumbnail secondary_logo',
+                'loading' => 'lazy',
+            ]) ?>
             <?= ImageGallery::widget([
                 'model' => $model,
-                'uploadFileName' => 'Secondary Logo',
+                'attribute' => 'secondary_logo',
                 'ajaxSuccess' => "
                     if(s.status == 'success') {
-                        KTApp.block('#secondary_logo-container', {
-                            overlayColor: '#000000',
-                            state: 'primary',
-                            message: 'Processing...'
-                        });
-                        setTimeout(function() {KTApp.unblock('#secondary_logo-container');}, 1000);
-                        $('#secondary_logo-container img').attr('src', s.src + '&w=200');
-                        $('#imagesettingform-secondary_logo').val(s.src);
+                        $('img.secondary_logo').attr('src', s.src + '&w=200');
                     }
                 ",
             ]) ?> 
 		</div>
 		<div class="col-md-4">
-           	<div class="mw200" id="favicon-container">
-                <p class="text-warning">Favicon</p>
-                <?= Html::image($model->favicon, ['w' => 200], [
-                    'class' => 'img-thumbnail',
-                    'loading' => 'lazy',
-                ]) ?>
-				<?= $form->field($model, 'favicon')->hiddenInput()->label(false) ?>
-            </div>
+            <p class="text-warning">Favicon</p>
+            <?= Html::image($model->favicon, ['w' => 200], [
+                'class' => 'img-thumbnail favicon',
+                'loading' => 'lazy',
+            ]) ?>
+			<?= $form->field($model, 'favicon')->hiddenInput()->label(false) ?>
             <?= ImageGallery::widget([
                 'model' => $model,
-                'uploadFileName' => 'Favicon',
+                'attribute' => 'favicon',
                 'ajaxSuccess' => "
                     if(s.status == 'success') {
-                        KTApp.block('#favicon-container', {
-                            overlayColor: '#000000',
-                            state: 'primary',
-                            message: 'Processing...'
-                        });
-                        setTimeout(function() {KTApp.unblock('#favicon-container');}, 1000);
-                        $('#favicon-container img').attr('src', s.src + '&w=200');
-                        $('#imagesettingform-favicon').val(s.src);
+                        $('img.favicon').attr('src', s.src + '&w=200');
                     }
                 ",
             ]) ?> 
@@ -90,27 +61,18 @@ use app\widgets\ImageGallery;
 	</div>
 	<div class="row">
 		<div class="col-md-4">
-          	<div class="mw200" id="image_holder-container">
-                <p class="text-warning">Image Holder</p>
-                <?= Html::image($model->image_holder, ['w' => 200], [
-                    'class' => 'img-thumbnail',
-                    'loading' => 'lazy',
-                ]) ?>
-				<?= $form->field($model, 'image_holder')->hiddenInput()->label(false) ?>
-            </div>
+            <p class="text-warning">Image Holder</p>
+            <?= Html::image($model->image_holder, ['w' => 200], [
+                'class' => 'img-thumbnail image_holder',
+                'loading' => 'lazy',
+            ]) ?>
+			<?= $form->field($model, 'image_holder')->hiddenInput()->label(false) ?>
             <?= ImageGallery::widget([
                 'model' => $model,
-                'uploadFileName' => 'Image Holder',
+                'attribute' => 'image_holder',
                 'ajaxSuccess' => "
                     if(s.status == 'success') {
-                        KTApp.block('#image_holder-container', {
-                            overlayColor: '#000000',
-                            state: 'primary',
-                            message: 'Processing...'
-                        });
-                        setTimeout(function() {KTApp.unblock('#image_holder-container');}, 1000);
-                        $('#image_holder-container img').attr('src', s.src + '&w=200');
-                        $('#imagesettingform-image_holder').val(s.src);
+                        $('img.image_holder').attr('src', s.src + '&w=200');
                     }
                 ",
             ]) ?> 
