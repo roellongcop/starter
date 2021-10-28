31ae00a2e1cdef748dad3ffabef77c6ee7a65661
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-10-28 14:53:48 +0800

    up

--- themes/keen/sub/demo1/noAsideLight/widgets/views/menu/_a_child.php
+++ themes/keen/sub/demo1/noAsideLight/widgets/views/menu/_a_child.php
@@ -5,12 +5,12 @@ use app\helpers\Html;
 
 <a href="<?= $menu['link'] ?? '' ?>" class="menu-link" 
 	target="<?= isset($menu['new_tab'])? '_blank': '_self' ?>">
-	<?= Html::if($withIcon, '
-		<span class="svg-icon menu-icon">
-	    	'.  $menu['icon'] ?? '' .'
-		</span>
-	') ?>
+	<?= Html::if($withIcon, 
+		'<span class="svg-icon menu-icon">
+	    	'.  ($menu['icon'] ?? '') .'
+		</span>'
+	) ?>
     <span class="menu-text">
-        <?= $menu['label'] ?? '' ?>
+        <?= ($menu['label'] ?? '') ?>
     </span>
 </a>
