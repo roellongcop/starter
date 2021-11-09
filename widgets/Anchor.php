6594f8f3617eef8b908500f0c47518a2c3e7cbe2
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2021-11-10 07:11:32 +0800

    Revert "Update Anchor.php"
    This reverts commit c9497aebe97495941f39a9780514d924d9db29fa.

--- widgets/Anchor.php
+++ widgets/Anchor.php
@@ -23,29 +23,27 @@ class Anchor extends BaseWidget
         // your logic here
         parent::init();
 
-        if (App::isLogin()) {
-            $request = new Request(['url' => parse_url(Url::to($this->link, true), PHP_URL_PATH)]);
-            $url = App::urlManager()->parseRequest($request);
-            list($controller, $actionID) = App::app()->createController($url[0]);
+        $request = new Request(['url' => parse_url(Url::to($this->link, true), PHP_URL_PATH)]);
+        $url = App::urlManager()->parseRequest($request);
+        list($controller, $actionID) = App::app()->createController($url[0]);
 
-            $this->controller = $controller ? $controller->id: '';
-            $this->action = $actionID;
+        $this->controller = $controller ? $controller->id: '';
+        $this->action = $actionID;
 
-            if (is_array($this->link)) {
-                $url = $this->link[0] ?? '';
+        if (is_array($this->link)) {
+            $url = $this->link[0] ?? '';
 
-                $explodedLink = explode('/', $url);
-                if (count($explodedLink) == 1) {
-                    $this->controller = $this->controller ?: App::controllerID();
-                    $this->action = $this->action ?: $explodedLink[0];
-                }
-                else {
-                    $this->controller = $this->controller ?: $explodedLink[0];
-                    $this->action = $this->action ?: $explodedLink[1];
-                }
+            $explodedLink = explode('/', $url);
+            if (count($explodedLink) == 1) {
+                $this->controller = $this->controller ?: App::controllerID();
+                $this->action = $this->action ?: $explodedLink[0];
             }
-            $this->user = $this->user ?: App::user();
+            else {
+                $this->controller = $this->controller ?: $explodedLink[0];
+                $this->action = $this->action ?: $explodedLink[1];
+            }
         }
+        $this->user = $this->user ?: App::user();
         $this->options['title'] = $this->options['title'] ?? $this->tooltip;
     }
 
@@ -55,10 +53,6 @@ class Anchor extends BaseWidget
      */
     public function run()
     {
-        if (App::isGuest()) {
-            return Html::a($this->title, $this->link, $this->options);
-        }
-        
         if ($this->link && !is_array($this->link)) {
             return Html::a($this->title, $this->link, $this->options);
         }
