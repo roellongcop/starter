b4c095f4c2aa95b8b1c996cd757e1ced3bb72ff8
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2022-02-03 07:10:56 +0800

    c

--- themes/keen/sub/demo1/main/views/layouts/_alert.php
+++ themes/keen/sub/demo1/main/views/layouts/_alert.php
@@ -3,14 +3,16 @@
 use app\helpers\Html;
 ?>
 <?= Html::foreach(Yii::$app->session->getAllFlashes(), function($message, $key) {
-	return '<div class="alert alert-custom alert-light-'. $key .' fade show mb-5" role="alert">
-	    <div class="alert-text">
-	    	'. $message .'
-	    </div>
-	    <div class="alert-close">
-	        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-	            <span aria-hidden="true"><i class="ki ki-close"></i></span>
-	        </button>
-	    </div>
-	</div>'
+	return <<< HTML
+			<div class="alert alert-custom alert-light-{$key} fade show mb-5" role="alert">
+			<div class="alert-text">
+				{$message}
+			</div>
+			<div class="alert-close">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true"><i class="ki ki-close"></i></span>
+				</button>
+			</div>
+		</div>
+	HTML;
 }) ?>
