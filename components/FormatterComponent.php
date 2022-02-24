333f4926a4c0381d5dc77141a26728d41bb8045e
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2022-02-24 15:03:34 +0800

    Update FormatterComponent.php

--- components/FormatterComponent.php
+++ components/FormatterComponent.php
@@ -107,28 +107,22 @@ class FormatterComponent extends \yii\i18n\Formatter
 
     public function asFileSize($bytes)
     {
-        if ($bytes >= 1073741824)
-        {
+        if ($bytes >= 1073741824) {
             $bytes = number_format($bytes / 1073741824, 2) . ' GB';
         }
-        elseif ($bytes >= 1048576)
-        {
+        elseif ($bytes >= 1048576) {
             $bytes = number_format($bytes / 1048576, 2) . ' MB';
         }
-        elseif ($bytes >= 1024)
-        {
+        elseif ($bytes >= 1024) {
             $bytes = number_format($bytes / 1024, 2) . ' KB';
         }
-        elseif ($bytes > 1)
-        {
+        elseif ($bytes > 1) {
             $bytes = $bytes . ' bytes';
         }
-        elseif ($bytes == 1)
-        {
+        elseif ($bytes == 1) {
             $bytes = $bytes . ' byte';
         }
-        else
-        {
+        else {
             $bytes = '0 bytes';
         }
 
