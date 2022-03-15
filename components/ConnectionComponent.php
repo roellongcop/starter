4923b5348eae7adfb5296b8ea12dc685f6aef60d
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2022-03-15 17:11:08 +0800

    Update ConnectionComponent.php

--- components/ConnectionComponent.php
+++ components/ConnectionComponent.php
@@ -6,7 +6,7 @@ class ConnectionComponent extends \yii\db\Connection
 {
     public $dsn = 'mysql:host=localhost;dbname=db_starter';
     public $username = 'root';
-    public $password = 'password';
+    public $password = '';
     public $charset = 'utf8';
     public $tablePrefix = 'tbl_';
 
