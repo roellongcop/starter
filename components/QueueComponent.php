b06a14bc8d9a37a8bf7d8ed8fcdce7cec8e19a41
Roel Longcop <51218281+roelfilweb@users.noreply.github.com>
2022-03-18 09:50:56 +0800

    c

--- components/QueueComponent.php
+++ components/QueueComponent.php
@@ -9,13 +9,7 @@ class QueueComponent extends \yii\queue\db\Queue
     public $db = 'db'; // DB connection component or its config 
     public $tableName = '{{%queues}}'; // Table name
     public $channel = 'default'; // Queue channel key
-    public $mutex = \yii\mutex\MysqlMutex::class; // Mutex that used to sync queries
+    public $mutex = 'yii\mutex\MysqlMutex'; // Mutex that used to sync queries
     public $ttr = 5 * 60; // Max time for job execution
     public $attempts = 3; // Max number of attempts
-
-    public function init()
-    {
-    	$this->tableName = Queue::tableName();
-        parent::init();
-    }
 }
