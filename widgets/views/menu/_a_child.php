dad52555a24f78837f549d07acf828df1fcdf89c
roellongcop <30338518+roellongcop@users.noreply.github.com>
2022-10-02 20:47:58 +0800

    Update _a_child.php

--- widgets/views/menu/_a_child.php
+++ widgets/views/menu/_a_child.php
@@ -1,4 +1,4 @@
-<a href="<?= $menu['link'] ?? '' ?>" class="menu-link" 
+<a href="<?= \app\helpers\App::baseUrl() . $menu['link'] ?? '' ?>" class="menu-link" 
 	target="<?= isset($menu['new_tab'])? '_blank': '_self' ?>">
 	<span class="svg-icon menu-icon">
     	<?= $menu['icon'] ?? '' ?>
