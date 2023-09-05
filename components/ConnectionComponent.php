34ffb355c75deab5721d051ab2b113b2e87429f1
roellongcop <longcoproel@gmail.com>
2023-09-05 18:15:51 +0800

    up

--- components/ConnectionComponent.php
+++ components/ConnectionComponent.php
@@ -4,7 +4,7 @@ namespace app\components;
 
 class ConnectionComponent extends \yii\db\Connection
 {
-    public $dsn = 'mysql:host=localhost;dbname=db_starter';
+    public $dsn = 'mysql:host=localhost;dbname=db_accounting';
     public $username = 'root';
     public $password = '';
     public $charset = 'utf8';
