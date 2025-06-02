4c743008545e68796b04ae211c1c6c582f4b4d71
Roel Longcop <30338518+roellongcop@users.noreply.github.com>
2025-06-02 18:40:08 +0800

    Update ConnectionComponent.php

--- components/ConnectionComponent.php
+++ components/ConnectionComponent.php
@@ -4,20 +4,19 @@ namespace app\components;
 
 class ConnectionComponent extends \yii\db\Connection
 {
-    /* Main Connection (Laragon configuration)
+    // Main Connection (Laragon configuration)
     public $dsn = 'mysql:host=localhost;dbname=db_starter';
     public $username = 'root';
     public $password = '';
     public $charset = 'utf8';
     public $tablePrefix = 'tbl_';
-    */
 
-    // Docker configuration for MariaDB connection
-    public $dsn = 'mysql:host=mariadb;port=3306;dbname=db_starter';
+    // Docker configuration for MariaDB connection(Docker)
+    /* public $dsn = 'mysql:host=mariadb;port=3306;dbname=db_starter';
     public $username = 'root';
     public $password = 'root_password';
     public $charset = 'utf8';
-    public $tablePrefix = 'tbl_';
+    public $tablePrefix = 'tbl_';*/
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
